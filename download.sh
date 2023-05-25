f691357f719a784d7442f5423901c482acefcccc
Nehanageswaran <neha.nageswaran@hof-university.de>
2023-05-26 00:30:44 +0200

    download helper script added.

---
+++ download.sh
@@ -0,0 +1,9 @@
+pip3 install gdown
+
+cd HR-VITON-main
+gdown 'https://drive.google.com/u/0/uc?id=1BkSA8UJo-6eOkKcXTFOHK80Esc4vBmVC&export=download'
+gdown 'https://drive.google.com/u/0/uc?id=1XJTCdRBOPVgVTmqzhVGFAgMm2NLkw5uQ&export=download'
+cd ..
+cd Graphonomy-master
+gdown 'https://drive.google.com/u/0/uc?id=1eUe18HoH05p0yFUd_sN6GXdTj82aW0m9&export=download'
+cd ..
